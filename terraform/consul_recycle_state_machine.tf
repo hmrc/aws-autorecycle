4d85c7ad1e6bf0fa951737f967cf1abe298d8e4e
Darren P <44468086+darrenprice12@users.noreply.github.com>
2025-07-07 15:33:57 +0100

    INFRA-8594. Add notifications for state function failure

--- terraform/consul_recycle_state_machine.tf
+++ terraform/consul_recycle_state_machine.tf
@@ -3,179 +3,191 @@ resource "aws_sfn_state_machine" "recycle_consul_agents" {
   role_arn = aws_iam_role.consul_step_machine.arn
 
   definition = <<EOF
+
   {
-    "StartAt": "StartNotification",
+    "StartAt": "MainFlow",
     "States": {
-      "StartNotification": {
-        "Type": "Task",
-        "Comment": "Send Slack notification that auto-recycling of the Consul Control plane has started",
-        "Resource": "${var.slack_notifications_lambda}",
-        "Parameters": {
-          "text": "Auto-recycling was successfully initiated",
-          "color": "good",
-          "message_content": {
-            "color": "good",
-            "text": "Auto-recycling of the Consul Control Plane was successfully initiated"
-          },
-          "username": "AutoRecycling"
-        },
-        "Next": "CheckClusterHealthInitial"
-      },
-      "CheckClusterHealthInitial": {
-        "Type": "Task",
-        "Comment": "Check that consul is healthy before we start. 1 leader and 2 followers totalling 3 members",
-        "Resource": "${module.CheckClusterHealth_lambda.lambda_alias_arn}",
-        "ResultPath": "$.initialHealth",
-        "Retry": [
+      "MainFlow": {
+        "Type": "Parallel",
+        "Branches": [
           {
-            "ErrorEquals": [
-              "States.ALL"
-            ],
-            "IntervalSeconds": 120,
-            "MaxAttempts": 4,
-            "BackoffRate": 2.0
-          }
-        ],
-        "Next": "GetConsulNodes"
-      },
-      "GetConsulNodes": {
-        "Type": "Task",
-        "Comment": "Gets the IP and instance ID for each consul agent. Returns a sorted array with the leader last",
-        "Resource": "${module.GetConsulNodes_lambda.lambda_alias_arn}",
-        "ResultPath": "$.nodes",
-        "Next": "ForEachInstance"
-      },
-      "ForEachInstance": {
-        "Type": "Map",
-        "Comment": "Process each node one at a time in the order determined by GetConsulNodes. So leader last",
-        "ItemsPath": "$.nodes.instanceList",
-        "MaxConcurrency": 1,
-        "Parameters": {
-          "ip.$": "$$.Map.Item.Value.ip",
-          "instanceId.$": "$$.Map.Item.Value.instanceId"
-        },
-        "Iterator": {
-          "StartAt": "GracefulLeave",
-          "States": {
-            "GracefulLeave": {
-              "Type": "Task",
-              "Comment": "Connect to each instance via SSM and issue the consul leave command",
-              "Resource": "arn:aws:states:::aws-sdk:ssm:sendCommand",
-              "Parameters": {
-                "DocumentName": "AWS-RunShellScript",
-                "InstanceIds.$": "States.Array($.instanceId)",
+            "StartAt": "StartNotification",
+            "States": {
+              "StartNotification": {
+                "Type": "Task",
+                "Comment": "Send Slack notification that auto-recycling of the Consul Control plane has started",
+                "Resource": "${var.slack_notifications_lambda}",
                 "Parameters": {
-                  "commands": [
-                    "curl -X PUT http://localhost:8500/v1/agent/leave"
-                  ]
-                }
+                  "text": "Auto-recycling was successfully initiated",
+                  "color": "good",
+                  "message_content": {
+                    "color": "good",
+                    "text": "Auto-recycling of the Consul Control Plane was successfully initiated"
+                  },
+                  "username": "AutoRecycling"
+                },
+                "Next": "CheckClusterHealthInitial"
               },
-              "ResultPath": "$.gracefulLeaveResult",
-              "Next": "WaitBeforeHealthCheck"
-            },
-            "WaitBeforeHealthCheck": {
-              "Type": "Wait",
-              "Comment": "Pause to allow node to leave cluster ",
-              "Seconds": 120,
-              "Next": "CheckClusterHealthPostLeave"
-            },
-            "CheckClusterHealthPostLeave": {
-              "Type": "Task",
-              "Comment": "Check the cluster is healthy and that there are now 2 nodes. 1 leader and one follower",
-              "Resource": "${module.CheckClusterHealth_lambda.lambda_alias_arn}",
-              "ResultPath": "$.healthCheck",
-              "Parameters": {
-                "expectedPeers": 2
+              "CheckClusterHealthInitial": {
+                "Type": "Task",
+                "Comment": "Check that consul is healthy before we start. 1 leader and 2 followers totalling 3 members",
+                "Resource": "${module.CheckClusterHealth_lambda.lambda_alias_arn}",
+                "ResultPath": "$.initialHealth",
+                "Retry": [
+                  {
+                    "ErrorEquals": ["States.ALL"],
+                    "IntervalSeconds": 120,
+                    "MaxAttempts": 4,
+                    "BackoffRate": 2.0
+                  }
+                ],
+                "Next": "GetConsulNodes"
               },
-              "Retry": [
-                {
-                  "ErrorEquals": [
-                    "States.ALL"
-                  ],
-                  "IntervalSeconds": 120,
-                  "MaxAttempts": 4,
-                  "BackoffRate": 2.0
-                }
-              ],
-              "Next": "TerminateNode"
-            },
-            "TerminateNode": {
-              "Type": "Task",
-              "Comment": "Terminate the node that has just left the consul control plane. ",
-              "Resource": "${module.TerminateConsulInstance_lambda.lambda_alias_arn}",
-              "Parameters": {
-                "instanceId.$": "$.instanceId"
+              "GetConsulNodes": {
+                "Type": "Task",
+                "Comment": "Gets the IP and instance ID for each consul agent. Returns a sorted array with the leader last",
+                "Resource": "${module.GetConsulNodes_lambda.lambda_alias_arn}",
+                "ResultPath": "$.nodes",
+                "Next": "ForEachInstance"
               },
-              "ResultPath": "$.terminateResult",
-              "Next": "WaitBeforeFinalHealthCheck"
-            },
-            "WaitBeforeFinalHealthCheck": {
-              "Type": "Wait",
-              "Comment": "Pause to allow ASG to replace terminated node ",
-              "Seconds": 300,
-              "Next": "CheckClusterHealthFinal"
-            },
-            "CheckClusterHealthFinal": {
-              "Type": "Task",
-              "Comment": "Confirm that cluster is stable with 3 nodes. 1 leader and 2 followers",
-              "Resource": "${module.CheckClusterHealth_lambda.lambda_alias_arn}",
-              "ResultPath": "$.postTerminationHealth",
-              "Retry": [
-                {
-                  "ErrorEquals": [
-                    "States.ALL"
-                  ],
-                  "IntervalSeconds": 180,
-                  "MaxAttempts": 6,
-                  "BackoffRate": 2.0
-                }
-              ],
-              "End": true
+              "ForEachInstance": {
+                "Type": "Map",
+                "Comment": "Process each node one at a time in the order determined by GetConsulNodes. So leader last",
+                "ItemsPath": "$.nodes.instanceList",
+                "MaxConcurrency": 1,
+                "Parameters": {
+                  "ip.$": "$$.Map.Item.Value.ip",
+                  "instanceId.$": "$$.Map.Item.Value.instanceId"
+                },
+                "Iterator": {
+                  "StartAt": "GracefulLeave",
+                  "States": {
+                    "GracefulLeave": {
+                      "Type": "Task",
+                      "Comment": "Connect to each instance via SSM and issue the consul leave command",
+                      "Resource": "arn:aws:states:::aws-sdk:ssm:sendCommand",
+                      "Parameters": {
+                        "DocumentName": "AWS-RunShellScript",
+                        "InstanceIds.$": "States.Array($.instanceId)",
+                        "Parameters": {
+                          "commands": [
+                            "curl -X PUT http://localhost:8500/v1/agent/leave"
+                          ]
+                        }
+                      },
+                      "ResultPath": "$.gracefulLeaveResult",
+                      "Next": "WaitBeforeHealthCheck"
+                    },
+                    "WaitBeforeHealthCheck": {
+                      "Type": "Wait",
+                      "Comment": "Pause to allow node to leave cluster ",
+                      "Seconds": 120,
+                      "Next": "CheckClusterHealthPostLeave"
+                    },
+                    "CheckClusterHealthPostLeave": {
+                      "Type": "Task",
+                      "Comment": "Check the cluster is healthy and that there are now 2 nodes. 1 leader and one follower",
+                      "Resource": "${module.CheckClusterHealth_lambda.lambda_alias_arn}",
+                      "ResultPath": "$.healthCheck",
+                      "Parameters": {
+                        "expectedPeers": 2
+                      },
+                      "Retry": [
+                        {
+                          "ErrorEquals": ["States.ALL"],
+                          "IntervalSeconds": 120,
+                          "MaxAttempts": 4,
+                          "BackoffRate": 2.0
+                        }
+                      ],
+                      "Next": "TerminateNode"
+                    },
+                    "TerminateNode": {
+                      "Type": "Task",
+                      "Comment": "Terminate the node that has just left the consul control plane.",
+                      "Resource": "${module.TerminateConsulInstance_lambda.lambda_alias_arn}",
+                      "Parameters": {
+                        "instanceId.$": "$.instanceId"
+                      },
+                      "ResultPath": "$.terminateResult",
+                      "Next": "WaitBeforeFinalHealthCheck"
+                    },
+                    "WaitBeforeFinalHealthCheck": {
+                      "Type": "Wait",
+                      "Comment": "Pause to allow ASG to replace terminated node",
+                      "Seconds": 300,
+                      "Next": "CheckClusterHealthFinal"
+                    },
+                    "CheckClusterHealthFinal": {
+                      "Type": "Task",
+                      "Comment": "Confirm that cluster is stable with 3 nodes. 1 leader and 2 followers",
+                      "Resource": "${module.CheckClusterHealth_lambda.lambda_alias_arn}",
+                      "ResultPath": "$.postTerminationHealth",
+                      "Retry": [
+                        {
+                          "ErrorEquals": ["States.ALL"],
+                          "IntervalSeconds": 180,
+                          "MaxAttempts": 6,
+                          "BackoffRate": 2.0
+                        }
+                      ],
+                      "End": true
+                    }
+                  }
+                },
+                "Next": "EndNotification"
+              },
+              "EndNotification": {
+                "Type": "Task",
+                "Comment": "Send notification that the step function has completed",
+                "Resource": "${var.slack_notifications_lambda}",
+                "Parameters": {
+                  "text": "Auto-recycling the consul cluster has completed",
+                  "color": "good",
+                  "message_content": {
+                    "color": "good",
+                    "text": "Auto-recycling of the Consul Control Plane was successfully completed"
+                  },
+                  "username": "AutoRecycling"
+                },
+                "End": true
+              }
             }
           }
-        },
-        "Next": "EndNotification"
+        ],
+        "Catch": [
+          {
+            "ErrorEquals": ["States.ALL"],
+            "Next": "FailureNotification"
+          }
+        ],
+        "Next": "SuccessEnd"
       },
-      "EndNotification": {
+      "FailureNotification": {
         "Type": "Task",
-        "Comment": "Send notification that the step function has completed",
+        "Comment": "Send Slack notification that the step function failed",
         "Resource": "${var.slack_notifications_lambda}",
         "Parameters": {
-          "text": "Auto-recycling the consul cluster has completed",
-          "color": "good",
+          "text": "Auto-recycling of the Consul Control Plane failed",
+          "color": "danger",
           "message_content": {
-            "color": "good",
-            "text": "Auto-recycling of the Consul Control Plane was successfully completed"
+            "color": "danger",
+            "text": "Auto-recycling of the Consul Control Plane encountered an error and was aborted"
           },
           "username": "AutoRecycling"
         },
         "End": true
+      },
+      "SuccessEnd": {
+        "Type": "Succeed"
       }
     }
   }
 
-EOF
 
-}
 
-resource "aws_iam_role" "consul_step_machine" {
-  name_prefix = "autorecycle-consul-step-"
 
-  permissions_boundary = var.account_engineering_boundary
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "states.${data.aws_region.current.name}.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
 EOF
 }
 
