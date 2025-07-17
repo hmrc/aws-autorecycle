bb2890cb283e07260dc6bfb7e9bc9e9207b45030
Mark Sawle <174456146+marksawle@users.noreply.github.com>
2025-07-17 13:04:09 +0100

    INFRA-8597 Multi-cluster support for consul recycle lambdas (#58)
    * INFRA-8597 Multi-cluster support for consul recycle lambdas
    
    * INFRA-8597 Satisfy linter
    
    * INFRA-8597 Satisfy linter (2)

--- src/get_consul_nodes/main.py
+++ src/get_consul_nodes/main.py
@@ -6,12 +6,21 @@ from typing import Any
 import boto3
 
 
-def lambda_handler(event: Any, context: Any) -> Any:
+# Normally we connect to consul-{environment}.{environment}.mdtp but we also support
+# connection to other clusters (e.g. engineer environments)
+def get_consul_host(event: Any) -> str:
     environment = os.environ.get("environment")
     if environment == "integration":
         print("Lambda Event Payload:")
         print(event)
-    consul_host = f"http://consul-{environment}.{environment}.mdtp:8500"
+
+    cluster = event.get("cluster", environment)
+    return f"http://consul-{cluster}.{environment}.mdtp:8500"
+
+
+def lambda_handler(event: Any, context: Any) -> Any:
+    consul_host = get_consul_host(event)
+
     ec2 = boto3.client("ec2")
 
     try:
